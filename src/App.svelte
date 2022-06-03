14ec16443d5ed5128632249549780d95f6ef5f27
Davor <davor.ornik@student.um.si>
2022-06-03 19:55:17 +0200

    CSS for small width screens.

--- src/App.svelte
+++ src/App.svelte
@@ -1,13 +1,10 @@
 <script>
-    import {width, height} from './game';
     import Canvas from './Canvas.svelte';
     import Background from './Background.svelte';
     import Selector from './Selector.svelte';
-    import Text from './Text.svelte';
     import FPS from './FPS.svelte';
     import Navbar from "./Navbar.svelte";
     import Boids from "./Boids.svelte";
-
 
 </script>
 
@@ -19,7 +16,7 @@
 
 <Navbar/>
 <Canvas>
-    <Background />
+    <Background/>
     <Boids/>
     <Selector/>
     <FPS/>
@@ -29,6 +26,6 @@
     :global(body) {
         margin: 0;
         padding: 0;
-        overflow: hidden;
+        overflow: clip;
     }
 </style>
--- src/Navbar.svelte
+++ src/Navbar.svelte
@@ -30,46 +30,53 @@
 
 <div class="navbar">
     <div class="grid">
-        <span class="row">
-            <span class="col">
-                <span class="row"><input type="number" on:change={() => changeSize() }
-                                         bind:value={size} min=1 name="boids"/></span>
-                <span class="row">boids</span>
-            </span>
-            <span class="col">
-                <span class="row"><input type="number" bind:value={$speed} min=1 max=10 name="speed"/></span>
-                <span class="row">speed</span>
-            </span>
-               <span class="col">
-                   <span class="row"><input type="number" bind:value={$force} min=0.01 max=1 name="speed"
-                                            step=".02"/></span>
-                   <span class="row">force</span>
+        <div class="tableAlign">
+            <span class="row">
+                <span class="col">
+                    <span class="row"><input type="number" on:change={changeSize} bind:value={size} min=1/></span>
+                    <span class="row">boids</span>
                 </span>
-            <span class="col">
-                <span class="row"><input type="number" bind:value={$view} min=1 max=360 name="speed"/></span>
-                <span class="row">view</span>
+                <span class="col">
+                    <span class="row"><input type="number" bind:value={$speed} min=1 max=10 name="speed"/></span>
+                    <span class="row">Speed</span>
+                </span>
+                   <span class="col">
+                       <span class="row"><input type="number" bind:value={$force} min=0.01 max=1 name="speed"
+                                                step=".02"/></span>
+                       <span class="row">Force</span>
+                    </span>
+                <span class="col">
+                    <span class="row"><input type="number" bind:value={$view} min=1 max=360 name="speed"/></span>
+                    <span class="row">View</span>
+                </span>
+                <span class="col">
+                    <span class="row"><input type="number" bind:value={$fSeek} min=1/></span>
+                    <span class="row">Seek</span>
+                </span>
+                <span class="col">
+                    <span class="row"><input type="number" bind:value={$fSeparate} min=1/></span>
+                    <span class="row">Separate</span>
+                </span>
+                <span class="col">
+                    <span class="row"><input type="number" bind:value={$fAlignment} min=1/></span>
+                    <span class="row">Alignment</span>
+                </span>
+                <span class="col">
+                    <span class="row"><input type="number" bind:value={$fCohesion} min=1/></span>
+                    <span class="row">Cohesion</span>
+                </span>
             </span>
-            <span class="col">
-                <span class="row"><input type="number" bind:value={$fSeek} min=1/></span>
-                <span class="row">Seek</span>
-            </span>
-            <span class="col">
-                <span class="row"><input type="number" bind:value={$fSeparate} min=1/></span>
-                <span class="row">Separate</span>
-            </span>
-            <span class="col">
-                <span class="row"><input type="number" bind:value={$fAlignment} min=1/></span>
-                <span class="row">Alignment</span>
-            </span>
-            <span class="col">
-                <span class="row"><input type="number" bind:value={$fCohesion} min=1/></span>
-                <span class="row">Cohesion</span>
-            </span>
-       </span>
+        </div>
+
     </div>
 </div>
 
 <style>
+    div {
+        margin: 0;
+        padding: 0;
+    }
+
     .navbar {
         background-color: #cccccc;
         height: calc(var(--navHeight) * 1px);
@@ -78,25 +85,36 @@
 
     .grid {
         display: table;
+        width: 100%;
+        white-space: nowrap;
         height: 100px;
+        user-select: none;
+    }
+
+    .tableAlign {
+        padding: 10px;
+        display: flex;
+        alignment: center;
         text-align: center;
+        vertical-align: middle;
+        justify-content: center;
     }
 
     .row {
         display: table-row;
         vertical-align: middle;
+        font-size: max(12px, min(1.1vw, 16px));
     }
 
     .col {
         display: table-cell;
         vertical-align: middle;
         text-align: center;
-        margin: auto;
+        margin: 0;
     }
 
     input[type='number'] {
-        width: auto;
-        max-width: 80px;
+        width: calc(100vw / 10);
         height: 30px;
     }
 
